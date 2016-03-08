f826bb201bf5c803e4afaa9581f883f034d1f54a
lexifdev <sl@lxf.kr>
2016-03-08 10:03:16 +0900

    map control 추가

--- src/JonmatMap.jsx
+++ src/JonmatMap.jsx
@@ -50,16 +50,25 @@ export default class JonmatMap extends React.Component {
     }
 
     componentDidMount() {
+        this.map = L.map(this.refs.map, {
+            attributionControl: false,
+            scrollWheelZoom: false,
+            zoomControl: false
+        });
+
+        let zoom = L.control.zoom({
+            position: 'bottomleft'
+        });
+
+        this.map.addControl(zoom);
+
         let tile = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
             minZoom: 0,
             maxZoom: 20,
             //detectRetina: true,
             ext: 'png'
         });
-        this.map = L.map(this.refs.map, {
-            attributionControl: false,
-            scrollWheelZoom: false
-        });
+
         this.map.addLayer(tile);
 
         this.map.setView([37.5207598,126.9209653], 13);
