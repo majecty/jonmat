37dbf515ae897a3b409a5b2b3d099ba130959d33
ParkJuhyung <majecty@gmail.com>
2021-10-08 23:51:11 +0900

    jsonmat2017_naver 시트에 맞게 데이터 뽑아내도록 변경
    ensure_ascii=False를 하면 json파일에 한글이 제대로 찍혀 나옴.

--- 01_get_data.py
+++ 01_get_data.py
@@ -9,18 +9,18 @@ cred = ServiceAccountCredentials.from_json_keyfile_name('key.json', [
 google_sheet = gspread.authorize(cred)
 
 doc = google_sheet.open_by_key('1vjCUzjeNxnNZ-ZaaNLM4fx2n_ephZDjpJSSvJGUVm0U')
-sheet = doc.worksheet('jonmat2017')
+sheet = doc.worksheet('jonmat2017_naver')
 
 data = []
-for man_name, party, restaurant_name, address, kind, price_str, memo in sheet.get_all_values()[1:]:
-    man_name = man_name.strip()
-    party = party.strip()
-    restaurant_name = restaurant_name.strip()
-    address = address.strip()
-    kind = kind.strip()
-    price = int(price_str.replace(',', ''))
+for row in sheet.get_all_values()[1:]:
+    man_name = row[2].strip()
+    party = row[3].strip()
+    restaurant_name = row[9].strip()
+    address = row[16].strip()
+    kind = row[10].strip()
+    price = int(row[8].replace(',', ''))
 
-    data.append((man_name, party, restaurant_name, address, kind, price, memo))
+    data.append((man_name, party, restaurant_name, address, kind, price, row[7]))
 
-print(json.dumps(data))
-json.dump(data, open('data/data.json', 'w+'))
+print(json.dumps(data), ensure_ascii=False)
+json.dump(data, open('data/data.json', 'w+'), ensure_ascii=False)
